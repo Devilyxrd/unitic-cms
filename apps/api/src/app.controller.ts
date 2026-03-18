b30a9a1c01a8b514c38b37fd2cf9657970f1cbee
Devilyxrd <devilyxrd@gmail.com>
2026-03-18 03:16:39 +0300

    feat: add ApiExcludeController decorator to AppController for Swagger documentation exclusion

--- apps/api/src/app.controller.ts
+++ apps/api/src/app.controller.ts
@@ -1,7 +1,9 @@
 import { Controller, Get } from '@nestjs/common';
+import { ApiExcludeController } from '@nestjs/swagger';
 import { Public } from './common/decorators/public.decorator';
 import { AppService } from './app.service';
 
+@ApiExcludeController()
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
